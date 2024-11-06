6be1a84d1b26b0f2d0eae479a0b36f6a369d7039
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-06 20:06:10 +0300

    Initial Commit

---
+++ adresDefteri.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Kisi
+{
+    // Özellikler
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public string TelefonNumarasi { get; set; }
+
+    // Yapıcı Metot
+    public Kisi(string ad, string soyad, string telefonNumarasi)
+    {
+        Ad = ad;
+        Soyad = soyad;
+        TelefonNumarasi = telefonNumarasi;
+    }
+
+    // Kisi Bilgisi Metodu
+    public string KisiBilgisi()
+    {
+        return $"Ad: {Ad} {Soyad}, Telefon: {TelefonNumarasi}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Kisi oluşturma
+        Kisi kisi1 = new Kisi("Furkan", "Akkamis", "0555 123 45 67");
+
+        // Kişinin bilgilerini alma
+        Console.WriteLine(kisi1.KisiBilgisi());
+    }
+}
