f401188422e63d8fa6877186f9e1fe55c63dd208
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-20 18:16:34 +0300

    Initial Commit

---
+++ BilgisayarIslemci/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Islemci
+{
+    public int Cekirdekler { get; set; }
+    public int Frekans { get; set; } // MHz cinsinden
+
+    public Islemci(int cekirdekler, int frekans)
+    {
+        Cekirdekler = cekirdekler;
+        Frekans = frekans;
+    }
+
+    public string IslemciBilgisi()
+    {
+        return $"İşlemci Çekirdek Sayısı: {Cekirdekler}, Frekans: {Frekans} MHz";
+    }
+}
+
+public class Bilgisayar
+{
+    public string Model { get; set; }
+    public Islemci Islemci { get; private set; }
+
+    public Bilgisayar(string model, int cekirdekler, int frekans)
+    {
+        Model = model;
+        Islemci = new Islemci(cekirdekler, frekans);
+    }
+
+    public string IslemciOlustur()
+    {
+        return $"Bilgisayar Modeli: {Model}, {Islemci.IslemciBilgisi()}";
+    }
+}
