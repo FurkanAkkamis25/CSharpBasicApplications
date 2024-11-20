514bb82068de88f19b3ba20539741670ff6a03dd
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-20 17:55:16 +0300

    Add files via upload

---
+++ YazarVeKitap/YazarVeKitap/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+public class Yazar
+{
+    public string Ad { get; set; }
+    public string Ulke { get; set; }
+    public List<Kitap> Kitaplar { get; set; }
+
+    public Yazar()
+    {
+        Kitaplar = new List<Kitap>();
+    }
+
+    public void KitapEkle(Kitap kitap)
+    {
+        Kitaplar.Add(kitap);
+    }
+}
+
+public class Kitap
+{
+    public string Baslik { get; set; }
+    public string ISBN { get; set; }
+}
