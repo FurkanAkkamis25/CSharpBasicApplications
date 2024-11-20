7791e01375821c482269e881d32dec97906af953
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-20 18:17:56 +0300

    Initial Commit

---
+++ OtomobilMotor/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Motor
+{
+    public int Güç { get; set; }
+    public string Tip { get; set; }
+
+    public Motor(int güç, string tip)
+    {
+        Güç = güç;
+        Tip = tip;
+    }
+
+    public string MotorBilgisi()
+    {
+        return $"Motor Gücü: {Güç}, Tip: {Tip}";
+    }
+}
+
+public class Otomobil
+{
+    public string Marka { get; set; }
+    public Motor Motor { get; private set; }
+
+    public Otomobil(string marka, int motorGüç, string motorTip)
+    {
+        Marka = marka;
+        Motor = new Motor(motorGüç, motorTip);
+    }
+
+    public string MotorOlustur()
+    {
+        return $"Otomobil Markası: {Marka}, {Motor.MotorBilgisi()}";
+    }
+}
