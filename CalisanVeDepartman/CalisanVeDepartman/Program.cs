2874eb2251ddeb51c5acba0f18c7b97b67250b90
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-20 17:57:24 +0300

    Initial Commit

---
+++ CalisanVeDepartman/CalisanVeDepartman/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Calisan
+{
+    public string Ad { get; set; }
+    public string Pozisyon { get; set; }
+    public Departman Departman { get; set; }
+
+    public void DepartmanAtama(Departman departman)
+    {
+        Departman = departman;
+    }
+}
+
+public class Departman
+{
+    public string Ad { get; set; }
+    public string Lokasyon { get; set; }
+}
