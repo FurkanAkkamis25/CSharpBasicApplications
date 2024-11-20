103ba8b32f010059d2ea6e89176d4aa5c2f30c89
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-20 17:59:07 +0300

    Initial Commit

---
+++ UrunVeSiparis/UrunVeSiparis/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+public class Urun
+{
+    public string Ad { get; set; }
+    public int Fiyat { get; set; }
+}
+
+public class Siparis
+{
+    public DateTime Tarih { get; set; }
+    public decimal Toplam { get; set; }
+    public List<Urun> UrunBilgisi { get; set; }
+
+    public Siparis()
+    {
+        UrunBilgisi = new List<Urun>();
+    }
+}
