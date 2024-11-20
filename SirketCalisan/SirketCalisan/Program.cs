6ee4a0ebb4e47b69d232cbc24cd0fe1939869b95
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-20 18:08:52 +0300

    Initial Commit

---
+++ SirketCalisan/SirketCalisan/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+public class Sirket
+{
+    public string Ad { get; set; }
+    public List<Calisan> Calisanlar { get; set; }
+
+    public Sirket()
+    {
+        Calisanlar = new List<Calisan>();
+    }
+
+    public void CalisanEkle(Calisan calisan)
+    {
+        Calisanlar.Add(calisan);
+    }
+}
+
+public class Calisan
+{
+    public string Ad { get; set; }
+    public string Pozisyon { get; set; }
+
+    public string CalisanBilgisi()
+    {
+        return $"{Ad} - {Pozisyon}";
+    }
+}
