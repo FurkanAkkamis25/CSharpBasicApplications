f6a6a4262e2fe372d7ae6de4a9f525576ce5ffd5
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-20 18:10:04 +0300

    Initial Commit

---
+++ KutuphaneKitap/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+public class Kutuphane
+{
+    public string Ad { get; set; }
+    public List<Kitap> Kitaplar { get; set; }
+
+    public Kutuphane()
+    {
+        Kitaplar = new List<Kitap>();
+    }
+
+    public void KitapEkle(Kitap kitap)
+    {
+        Kitaplar.Add(kitap);
+    }
+}
+
+public class Kitap
+{
+    public string Baslik { get; set; }
+    public string Yazar { get; set; }
+}
