8426ebfbcfe7deeccb1e5c5c40620356f655c307
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-20 18:07:34 +0300

    Initial Commit

---
+++ EvOda/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+public class Ev
+{
+    public string Ad { get; set; }
+    public List<Oda> Odalar { get; set; }
+
+    public Ev()
+    {
+        Odalar = new List<Oda>();
+    }
+
+    public void OdaEkle(Oda oda)
+    {
+        Odalar.Add(oda);
+    }
+}
+
+public class Oda
+{
+    public string Boyut { get; set; }
+    public string Tip { get; set; }
+}
