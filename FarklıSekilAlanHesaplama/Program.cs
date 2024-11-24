5f9a531402d10d5999b3ce54fc89e480a70e670d
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-24 15:20:58 +0300

    Initial Commit

---
+++ FarklıSekilAlanHesaplama/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    //Karenin alanı
+    static double Alan(double kenar)
+    {
+        return kenar * kenar;
+    }
+
+    //Dikdörtgenin alanı
+    static double Alan(double uzunKenar, double kisaKenar)
+    {
+        return uzunKenar * kisaKenar;
+    }
+
+    // Dairenin alanı
+    static double Alan(double yaricap, bool daire)
+    {
+        if (daire) // Dairenin alanı için ek parametre kontrolü
+            return Math.PI * yaricap * yaricap;
+        return 0;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Karenin Alanı: " + Alan(4)); 
+        Console.WriteLine("Dikdörtgenin Alanı: " + Alan(5, 3));
+        Console.WriteLine("Dairenin Alanı: " + Alan(3, true)); 
+    }
+}
