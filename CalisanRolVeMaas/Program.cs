d907cd4ad86527f17db70fb7daed4a6274fe295a
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-26 14:06:21 +0300

    Initial Commit

---
+++ CalisanRolVeMaas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+enum EmployeeRole
+{
+    Manager,
+    Developer,
+    Designer,
+    Tester
+}
+
+class SalaryCalculator
+{
+    public static int CalculateSalary(EmployeeRole role)
+    {
+        switch (role)
+        {
+            case EmployeeRole.Manager:
+                return 10000;
+            case EmployeeRole.Developer:
+                return 8000;
+            case EmployeeRole.Designer:
+                return 7000;
+            case EmployeeRole.Tester:
+                return 6000;
+            default:
+                throw new ArgumentException("Geçersiz rol.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Manager maaşı: {SalaryCalculator.CalculateSalary(EmployeeRole.Manager)} TL");
+        Console.WriteLine($"Developer maaşı: {SalaryCalculator.CalculateSalary(EmployeeRole.Developer)} TL");
+        Console.WriteLine($"Designer maaşı: {SalaryCalculator.CalculateSalary(EmployeeRole.Designer)} TL");
+        Console.WriteLine($"Tester maaşı: {SalaryCalculator.CalculateSalary(EmployeeRole.Tester)} TL");
+    }
+}
