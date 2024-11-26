2917da0b8359d21cf536ffe0a1e871b0ab6b1aaf
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-26 14:04:52 +0300

    Initial Commit

---
+++ HavaDurumuTahmini/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+enum Weather
+{
+    Sunny,
+    Rainy,
+    Cloudy,
+    Stormy
+}
+
+class WeatherAdvice
+{
+    public static string GetAdvice(Weather weather)
+    {
+        switch (weather)
+        {
+            case Weather.Sunny:
+                return "Güneş kremi sür!";
+            case Weather.Rainy:
+                return "Şemsiyeni al!";
+            case Weather.Cloudy:
+                return "Hava serin olabilir, dikkat et.";
+            case Weather.Stormy:
+                return "Dışarı çıkma, güvende kal.";
+            default:
+                return "Geçersiz hava durumu.";
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(WeatherAdvice.GetAdvice(Weather.Sunny));
+        Console.WriteLine(WeatherAdvice.GetAdvice(Weather.Rainy));
+        Console.WriteLine(WeatherAdvice.GetAdvice(Weather.Cloudy));
+        Console.WriteLine(WeatherAdvice.GetAdvice(Weather.Stormy));
+    }
+}
