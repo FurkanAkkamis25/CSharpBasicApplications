25f4901ef53943af11353ec64906758241ebde65
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-26 14:03:31 +0300

    Initial Commit

---
+++ TrafikIsiği/TrafikIsiği/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+enum TrafficLight
+{
+    Red,
+    Yellow,
+    Green
+}
+
+class TrafficSignal
+{
+    public static string GetAction(TrafficLight light)
+    {
+        switch (light)
+        {
+            case TrafficLight.Red:
+                return "Dur!";
+            case TrafficLight.Yellow:
+                return "Hazır ol!";
+            case TrafficLight.Green:
+                return "Geç!";
+            default:
+                return "Geçersiz trafik ışığı.";
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(TrafficSignal.GetAction(TrafficLight.Red));
+        Console.WriteLine(TrafficSignal.GetAction(TrafficLight.Yellow));
+        Console.WriteLine(TrafficSignal.GetAction(TrafficLight.Green));
+    }
+}
